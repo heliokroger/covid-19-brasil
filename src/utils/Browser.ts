8dfbc660e5671d34ab16075d58d49632b2bc47db
Hélio Júnior Rodrigues Kroger <heliojuniorrodrigueskroger@MacBook-Pro-de-Helio.local>
2020-06-23 12:03:38 -0300

    adding --no-sandbox

--- src/utils/Browser.ts
+++ src/utils/Browser.ts
@@ -4,7 +4,10 @@ export default class BrowserService {
     public static instance: puppeteer.Browser;
 
     public static async open() {
-        this.instance = await puppeteer.launch();
+        this.instance = await puppeteer.launch({
+            headless: true,
+            args: ['--no-sandbox'],
+        });
     }
 
     public static close() {
